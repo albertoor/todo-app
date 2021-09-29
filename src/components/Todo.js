43deb5c256df8563cc89550c2c3358340b18d8be
albertoornelasdev <albertoornelasdev@gmail.com>
2021-09-28 22:48:03 -0700

    todo styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { db } from '../db/firebase';
 import firebase from 'firebase/compat/app';
 import swal from 'sweetalert';
+import styled from 'styled-components';
+import { AiFillDelete } from 'react-icons/ai';
 
 export default function Todo({ id, todo, timestamp, completed, userId }) {
   const deleteTodo = async () => {
@@ -43,14 +45,59 @@ export default function Todo({ id, todo, timestamp, completed, userId }) {
 
   return (
     <>
-      <input
-        type="checkbox"
-        onClick={() => checkedTodo()}
-        defaultChecked={completed ? false : true}
-      />
-      <p>{todo}</p>
-      <span>{new Date(timestamp.toDate()).toUTCString()}</span>
-      <button onClick={deleteTodo}>X</button>
+      <TodoContainer>
+        <Input
+          type="checkbox"
+          onClick={() => checkedTodo()}
+          defaultChecked={completed ? false : true}
+        />
+        {completed ? (
+          <Text>{todo}</Text>
+        ) : (
+          <TextLineTrough>{todo}</TextLineTrough>
+        )}
+        {completed ? (
+          <Text>{new Date(timestamp.toDate()).toUTCString()}</Text>
+        ) : (
+          <TextLineTrough>
+            {new Date(timestamp.toDate()).toUTCString()}
+          </TextLineTrough>
+        )}
+        <DeleteBtn onClick={deleteTodo}>
+          <AiFillDelete color="#ffff" />
+        </DeleteBtn>
+      </TodoContainer>
     </>
   );
 }
+
+const TodoContainer = styled.div`
+  margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  margin-left: 0.5rem;
+`;
+
+const TextLineTrough = styled.p`
+  margin-left: 0.5rem;
+  text-decoration: line-through;
+`;
+
+const Input = styled.input`
+  display: flex;
+  align-items: center;
+`;
+
+const DeleteBtn = styled.button`
+  margin-left: 0.5rem;
+  display: flex;
+  padding: 0.5rem 0.5rem;
+  border-radius: 5px;
+  background-color: #ff6961;
+  cursor: pointer;
+  border: none;
+  align-self: center;
+`;
